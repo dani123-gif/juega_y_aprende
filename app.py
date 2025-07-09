5b05185450302fc18659fafeebc29ecc48f01781
Daniel Mena <dani1234hjk@gmail.com>
2025-07-09 15:27:37 -0500

    fix: escuchar en 0.0.0.0 y puerto de Render

--- app.py
+++ app.py
@@ -194,6 +194,8 @@ def historial():
     return render_template('historial.html', historial=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
